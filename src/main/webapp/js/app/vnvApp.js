b0e439243abe3285f7a51a1ebe2e37acaa1b1877
Patrick Treyer <p.treyer@web.de>
2016-11-06 10:39:17 +0100

    adds form for registration and login

--- src/main/webapp/js/app/vnvApp.js
+++ src/main/webapp/js/app/vnvApp.js
@@ -2,7 +2,10 @@ var vnvApp = angular.module('vnvApp', [
     'ngResource', 'ngRoute']);
 
 vnvApp.config([ '$routeProvider', function($routeProvider) {
-    $routeProvider.when('/Register', {
+    $routeProvider.when('/', {
+        templateUrl : 'main.html',
+        controller : 'MainCtl'
+    }).when('/Register', {
         templateUrl : 'register.html',
         controller : 'RegistrationCtl'
     }).otherwise({
