f9adfa2bc847a290e7456fe87cba5ac2762d31f8
Waldemar Merk <merk.waldemar@gmail.com>
2016-11-11 13:32:44 +0100

    adds tests

--- src/test/java/com/vnv/Cucumber/RegisterDefs.java
+++ src/test/java/com/vnv/Cucumber/RegisterDefs.java
@@ -2,26 +2,31 @@ package com.vnv.Cucumber;
 
 
 import Configuration.BasedriverConfiguration;
+import cucumber.api.PendingException;
 import cucumber.api.java.After;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.hamcrest.Matchers;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Action;
-import org.openqa.selenium.interactions.Actions;
 
 import java.util.Random;
 
 import static org.junit.Assert.assertThat;
+import static org.openqa.selenium.By.className;
 import static org.openqa.selenium.By.id;
+import static org.openqa.selenium.By.tagName;
 
 public class RegisterDefs extends BasedriverConfiguration {
 
     private WebDriver browser;
 
+
+
     @Given("^the username is \"([^\"]*)\" and the email is \"([^\"]*)\" and the password \"([^\"]*)\"$")
+
     public void the_username_is_something_and_the_email_is_something_and_the_password_something(String username, String email, String password) throws Throwable {
         browser = webDriver();
         browser.get("localhost:8080/#/Register");
@@ -41,18 +46,12 @@ public class RegisterDefs extends BasedriverConfiguration {
 
 
     @When("^the register button is clicked$")
+
     public void the_register_button_is_clicked() throws Throwable {
         Thread.sleep(1000);
-//        WebElement registerForm = browser.findElement(id("registerForm"));
-//        registerForm.submit();
-        WebElement submitButton = browser.findElement(id("signUp"));
-
-        submitButton.click();
-//        Actions ob = new Actions(browser);
-//        ob.click(submitButton);
-//        Action action  = ob.build();
-//        action.perform();
-
+        WebElement registerForm = browser.findElement(id("signUp"));
+        registerForm.click();
+        Thread.sleep(2000);
     }
 
 
@@ -61,23 +60,36 @@ public class RegisterDefs extends BasedriverConfiguration {
 
     public void theRegisterPageIsShownAndTheCssSelectorIsAndTheMessageIs(String cssSelector, String message) throws Throwable {
 
-        Thread.sleep(2000);
-        //browser.get("localhost:8080/#/Main");
         WebElement element = browser.findElement(id("successfulRegistration"));
         String text = element.getText();
         assertThat(text.contains(message), Matchers.is(true));
     }
 
+
+
     @After
+
     public void tearDown() throws Exception {
+
         closeBrowser();
+
     }
 
+
+
     @Then("^the register page is shown$")
+
     public void theRegisterPageIsShown() throws Throwable {
-        Thread.sleep(1000);
-        WebElement registerForm = browser.findElement(id("register-button"));
-        String disabled = registerForm.getAttribute("disabled");
-        assertThat(disabled, Matchers.notNullValue());
+
+        WebElement registerForm = browser.findElement(id("form-firstname"));
+        String username = registerForm.getText();
+        assertThat(username, Matchers.isEmptyOrNullString());
+    }
+
+    @Then("^an Registrationerror with the message \"([^\"]*)\" is shown$")
+    public void anRegistrationerrorWithTheMessageIsShown(String message) throws Throwable {
+        WebElement registerError = browser.findElement(id("errorAlreadyRegistered"));
+        String text = registerError.getText();
+        assertThat(message,Matchers.is(text));
     }
 }
