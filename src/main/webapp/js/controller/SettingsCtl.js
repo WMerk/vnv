fe0308fc661f56307a665062f635dd163930d189
Patrick Treyer <p.treyer@web.de>
2016-11-19 00:33:27 +0100

    śolves bug in registration succeeded dialogue

--- src/main/webapp/js/controller/SettingsCtl.js
+++ src/main/webapp/js/controller/SettingsCtl.js
@@ -7,6 +7,7 @@ vnvApp.controller(
         function ($scope, $location, userService) {
 
             $scope.init = function () {
+                userService.setNewUser(false);
                 $scope.user = userService.getCurrentUser();
                 $scope.authenticated = userService.getAuthenticated();
                 $scope.template = 'navigation.html';
