5b08f856ba0ca0880b714479f90072c26a7788af
Waldemar Merk <merk.waldemar@gmail.com>
2016-11-19 17:00:39 +0100

    changes test sequence in testing. First unittests then other tests

--- src/test/java/com/vnv/ZCucumber/LoginSteps.java
+++ src/test/java/com/vnv/ZCucumber/LoginSteps.java
@@ -1,7 +1,6 @@
-package com.vnv.Cucumber;
+package com.vnv.ZCucumber;
 
 import Configuration.BasedriverConfiguration;
-import cucumber.api.PendingException;
 import cucumber.api.java.After;
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
