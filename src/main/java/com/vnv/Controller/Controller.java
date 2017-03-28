3ca4dc67b1af43a082f2876adb6167c93d1772e6
Zachmann <gabriel.zachmann@kit.edu>
2017-03-28 16:22:59 +0200

    google login first draft #vnv-72

--- src/main/java/com/vnv/Controller/Controller.java
+++ src/main/java/com/vnv/Controller/Controller.java
@@ -1,12 +1,24 @@
 package com.vnv.Controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpSession;
+import java.math.BigInteger;
+import java.security.SecureRandom;
 
 @org.springframework.stereotype.Controller
 public class Controller {
 
+    private static Logger log = LoggerFactory.getLogger(Controller.class);
+
     @RequestMapping(value = "/{[path:[^\\.]*}")
-    public String redirect() {
+    public String redirect(HttpSession session) {
+        String state = new BigInteger(130, new SecureRandom()).toString(32);
+        session.setAttribute("state", state);
+        log.debug(state);
         return "forward:/";
     }
+
 }
