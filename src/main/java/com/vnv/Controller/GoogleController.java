a82e1d249ad5b5a400ef79be31b99419947b5894
Gabriel Zachmann <gabriel.zachmann@kit.edu>
2017-03-29 10:10:30 +0200

    when registering with google the users google contacts are friend requested #vnv-72

--- src/main/java/com/vnv/Controller/GoogleController.java
+++ src/main/java/com/vnv/Controller/GoogleController.java
@@ -1,16 +1,31 @@
 package com.vnv.Controller;
 
+import com.vnv.Dao.UserDao;
+import com.vnv.Dao.UserRelDao;
 import com.vnv.Entity.User;
 import com.vnv.Model.Http;
+import org.apache.http.Header;
 import org.apache.http.NameValuePair;
+import org.apache.http.message.BasicHeader;
 import org.apache.http.message.BasicNameValuePair;
 import org.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
 
 import javax.servlet.http.HttpSession;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 import java.io.IOException;
+import java.io.StringReader;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
@@ -20,8 +35,13 @@ public class GoogleController {
 
     private static Logger log = LoggerFactory.getLogger(GoogleController.class);
 
+    @Autowired
+    private UserDao userDao;
+    @Autowired
+    private UserRelDao userRelDao;
+
     @RequestMapping(value="/gconnect")
-    String gconnect(@RequestParam String code, @RequestParam String state, HttpSession session) throws IOException {
+    String gconnect(@RequestParam String code, @RequestParam String state, HttpSession session) throws IOException, ParserConfigurationException, SAXException {
         log.debug(state);
         log.debug(code);
         if (!state.equals(session.getAttribute("state"))) {
@@ -65,7 +85,33 @@ public class GoogleController {
 
         //TODO check if user is registered (by googleid), if ->login, if not-> register user, if account already exists for that email there should be some possibility to combine them (IDH)
 
+        // gets Friends and send request
+        List<User> gfriends = getFriends(user.getMail(), access_token);
+        for (User gfriend:gfriends) {
+            userRelDao.addRequest(user, gfriend);
+        }
+
         return user.toJSON().toString();
+    }
+
+    public List<User> getFriends(String mail, String access_token) throws IOException, ParserConfigurationException, SAXException {
+        Header header = new BasicHeader("Authorization", "Bearer "+access_token);
+        String url = String.format("https://www.google.com/m8/feeds/contacts/%s/full",mail);
+        String response = Http.get(url, header);
+
+        Document doc = DocumentBuilderFactory.newInstance().newDocumentBuilder()
+                .parse(new InputSource(new StringReader(response)));
+
+        List<User> gfriends = new ArrayList<>();
+        NodeList mailNodes = doc.getElementsByTagName("gd:email");
+        for(int i=0; i< mailNodes.getLength(); i++) {
+            Element element = (Element) mailNodes.item(i);
+            User friend = userDao.getUserByMail(element.getAttribute("address"));
+            if (friend!=null) {
+                gfriends.add(friend);
+            }
+        }
+        return gfriends;
     }
 
     public boolean validate(String iss, String aud, long exp) {
--- src/main/java/com/vnv/Model/Http.java
+++ src/main/java/com/vnv/Model/Http.java
@@ -2,11 +2,13 @@ package com.vnv.Model;
 
 
 import org.apache.commons.io.IOUtils;
+import org.apache.http.Header;
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
 import org.apache.http.NameValuePair;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.HttpClients;
 import org.json.JSONObject;
@@ -15,7 +17,6 @@ import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
 import java.io.InputStream;
-
 import java.util.List;
 
 public class Http {
@@ -37,6 +38,26 @@ public class Http {
                 JSONObject json = new JSONObject(IOUtils.toString(instream, "UTF-8"));
                 log.debug(json.toString());
                 return json;
+            } finally {
+                instream.close();
+            }
+        }
+        return null;
+    }
+
+    public static String get(String url, Header header) throws IOException {
+        HttpClient httpclient = HttpClients.createDefault();
+        HttpGet get = new HttpGet(url);
+        get.setHeader(header);
+
+        HttpResponse response = httpclient.execute(get);
+        HttpEntity entity = response.getEntity();
+        if (entity != null) {
+            InputStream instream = entity.getContent();
+            try {
+                String responseString = IOUtils.toString(instream, "UTF-8");
+                log.debug(responseString);
+                return responseString;
             } finally {
                 instream.close();
             }
